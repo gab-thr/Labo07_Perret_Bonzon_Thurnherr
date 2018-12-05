3335fd0011280a4568e886cf86d8c1307376104d
Benoit P <10925370+nwabeen@users.noreply.github.com>
2018-12-05 10:34:52 +0100

    Ajout de la structure de base

---
+++ main.cpp
@@ -0,0 +1,34 @@
+/*
+    -----------------------------------------------------------------------------------
+    Laboratoire : Labo07
+    Fichier     : main.cpp
+    Auteur(s)   : Ludovic Bonzon, Benoit Perret, Gabrielle Thurnherr
+    Date        : 28.11.2018
+
+    But         : Vous devez écrire un programme convertissant les nombres écrits avec
+    le système de numérotation indo-arabe en nombres romains et vice-versa. Il lit les
+    entrées ligne par ligne et détecte dans quel sens doit s'effectuer la conversion.
+    Si l'entrée est non valide, il écrit "Non valide". Il s'arrête quand il reçoit une
+    ligne vide. Il doit être capable de traiter au moins des nombres entre I et MMMMCMXCIX,
+    donc entre 1 et 4999.
+    Bien que l'usage romain varie, nous ne considérons ici comme valide que les nombres
+    romains écrits sans symbole se répétant plus de 3 fois, à l'exception du M. Ainsi 4
+    s'écrit IV et pas IIII, 99 s'écrit XCIX et pas LXXXXVIIII. Les nombres romains ne
+    respectant pas cette règle doivent être considérés non valides.
+
+    Remarque(s) :
+
+    Compilateur : MinGW-g++ <6.3.0>
+    -----------------------------------------------------------------------------------
+    */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  
+
+    return 0;
+}
